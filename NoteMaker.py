385de50511a2dc45c5ed2ad9238022a02177bcac
ShayakSarkar <shayaksarkar2000@gmail.com>
2020-07-15 00:04:43 +0530

    added references

--- NoteMaker.py
+++ NoteMaker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys,os
+import copy as cp,sys,os
 
 class Node:
     def __init__(self,node_type,data):
@@ -7,34 +7,48 @@ class Node:
         self.data=data
         self.children=[]
 
+references=dict()   #A dictionary of references created. For quickly looking up nodes
+
 class Tree:
     def __init__(self):
         self.root=None
          
     def print_tree(self,node,indent):
-        #print(indent+node.type,node.data)
+        print(indent+node.type,node.data)
         for i in node.children:
             self.print_tree(i,indent+"\t")
 
-    def insert_line_helper(self,node,data,level_ctr,cur_level):
+    def insert_line_helper(self,node,data,level_ctr,cur_level,ref):
         if level_ctr==(cur_level+1):
             node.children.append(Node('L',data))
-            return
+            #print(node.children[-1].data)
+            retval=node.children[-1]
+            #print(retval)
+            if ref!=None:
+                references[ref]=retval
+            return cp.copy(retval)
+
         if len(node.children)==0:
-            #print('Syntax error: Level exceeded')
+            print('Syntax error: Level exceeded')
             return
  
-        self.insert_line_helper(node.children[-1],data,level_ctr,cur_level+1)
+        self.insert_line_helper(node.children[-1],data,level_ctr,cur_level+1,ref)
      
-    def insert_header_helper(self,node,data,level_ctr,cur_level):
+    def insert_header_helper(self,node,data,level_ctr,cur_level,ref):
         if level_ctr==(cur_level+1):
             node.children.append(Node('H',data))
-            return
+            #print(node.children[-1].data)
+            retval=node.children[len(node.children)-1]
+            #print(retval)
+            if ref!=None:
+                references[ref]=retval
+            return cp.copy(retval)
+
         if len(node.children)==0:
-            #print('Syntax error: Level exceeded')
+            print('Syntax error: Level exceeded')
             return
  
-        self.insert_header_helper(node.children[-1],data,level_ctr,cur_level+1)
+        self.insert_header_helper(node.children[-1],data,level_ctr,cur_level+1,ref)
 
     def insert_continuation_helper(self,node,data,level_ctr,cur_level):
         if level_ctr==(cur_level+1):
@@ -51,16 +65,21 @@ class Tree:
 
 
     
-    def insert_header(self,data,level_ctr):
+    def insert_header(self,data,level_ctr,ref):
+
         if self.root==None:
             self.root=Node('H',data)
             #print('Root added')
             return
-        self.insert_header_helper(self.root,data,level_ctr,1)
+        node=self.insert_header_helper(self.root,data,level_ctr,1,ref)
+        #print(node)
+        return node
         #print('header',data,'inserted')
 
-    def insert_line(self,data,level_ctr):
-        self.insert_line_helper(self.root,data,level_ctr,1)
+    def insert_line(self,data,level_ctr,ref):
+        node=self.insert_line_helper(self.root,data,level_ctr,1,ref)
+        #print(node)
+        return node
         #print('line',data,'inserted')
 
     def insert_continuation(self,data,level_ctr):
@@ -84,6 +103,12 @@ def make_tree(ifile):
             #by the note taker for clarity
             continue
         
+        if line[0]=='@':
+            #reference to a previously defined reference
+            ref,line=line.split()[0][1:],' '.join(line.split()[1:])
+            references[ref].children.append(Node('L',line))
+            continue 
+
         level_ctr=0
         for i in line:
             if i=='>':
@@ -93,6 +118,15 @@ def make_tree(ifile):
         
         line=line[level_ctr:]
 
+        ref=None
+        ind=None
+        try:
+            ind=line.index('@')
+            ref=line[(ind+1):]
+            line=line[:ind].strip()
+        except:
+            pass
+
         if level_ctr==0:    #If it is a cont then it is not a new header or line
             level_ctr=prev_level_ctr
             #print('Continuation detected:',line)
@@ -100,11 +134,13 @@ def make_tree(ifile):
             tree.insert_continuation(line,prev_level_ctr)  
         
         elif is_header(line):   
+            
             line=line[2:-2]
-            tree.insert_header(line,level_ctr)
-        
+            node=tree.insert_header(line,level_ctr,ref)
+
         else:   #It is a normal line
-            tree.insert_line(line,level_ctr)
+            node=tree.insert_line(line,level_ctr,ref)
+
         prev_level_ctr=level_ctr
         
     return tree
@@ -116,22 +152,25 @@ input_file=sys.argv[1]
 #print('Input File:',input_file,'Output File:',op_file)
 
 ifile=open(input_file,"r")
-
 #print('Attempting to make tree')
 
 #Convert the input file into a tree
 tree=make_tree(ifile)
 
+#print(references)
+#for i in references:
+#    print(references[i].data)
 #print('Tree made')
 #print('Basic view\n')
 #tree.print_tree(tree.root,'')
 
 #Now we have a tree representation of the entire document
 #Now we can simply start off rendering the tree appropriately 
-
+#We also have a list of references as requried by the user
 tabwidth=8
 
 print('Made with NoteMaker')
+
 def render(node,indent,j_fact):
  
     #indent is the number of tabspaces (of size tabwidth) to be applied
